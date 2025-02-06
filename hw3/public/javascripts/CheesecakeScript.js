c6f3fe3172af3e3d3a4cbfaeb9b86212a101d646
Rowena <archerr27@up.edu>
2025-02-05 18:22:45 -0800

    Got all the Node.js stuff working Changed cheesecake html files to separate javascript

---
+++ hw3/public/javascripts/CheesecakeScript.js
@@ -0,0 +1,34 @@
+// java script stuff
+// Author: Rowena Archer
+    $(document).ready(function(){
+
+        //script for order button
+        $(":button").click(function(){
+            // record values from input
+            let notesText = $("#notes").val();
+            let quant = $("#Quantity").val();
+            let topping = $('input[name="topping"]:checked').val();
+            
+            // send alert if user inputs vegan
+            if (notesText.includes("vegan") || notesText.includes("Vegan")){
+                alert("Our cheesecakes contain dairy and are not vegan.")
+            }
+            else { // process order, reiterate order
+            $(".Order").hide();
+            $("#Change").replaceWith("Thank you! Your order has been placed. Quantity: " + quant + ", Toppings: " + topping + ", Additional notes: " + notesText)
+            }
+        });
+
+        // script for dropdown menu
+        $("#menu-options a").click(function(event) {
+            event.preventDefault();
+            var selection = $(this).text(); // Get the selected option text
+
+            // Update the button text
+            $("#menu-btn").text(selection);
+
+            // Close the dropdown
+            $("#menu-options").removeClass("show");
+        });
+
+    });
