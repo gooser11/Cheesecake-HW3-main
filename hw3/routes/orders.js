a59828fb414ff4a0a62dae1a414de1b6dedb8bfb
Rowena <archerr27@up.edu>
2025-02-26 18:31:44 -0800

    JSON array made and sends

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+//JSON array
+const orders = [
+    { topping: 'Cherry', quantity: 2 },
+    { topping: 'Chocolate', quantity: 1 },
+  ];
+
 router.get('/', (req, res) => {
-    res.send('Orders Page');
+    res.send(orders); //send the mf JSON
 });
 
 module.exports = router;
